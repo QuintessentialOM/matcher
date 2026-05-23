798274a0babebe18c2699b30bdc57073453fdd2f
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2026-05-23 12:24:36 +1200

    emit mappings based on matches

--- Matcher/Main.cs
+++ Matcher/Main.cs
@@ -1,6 +1,10 @@
 global using System;
 global using System.Collections.Generic;
 global using Mono.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Matcher.Matching;
 using Matcher.Matching.Classifier;
 using Mono.Cecil;
 
@@ -20,5 +24,68 @@ public static class MatcherMain {
 				"#=qDID3KRmTOKqTiWqrwHq$pA==",
 				["strings_old.csv"], ["strings_ce_skew_polymers.csv"]);
 		matcher.AutoMatchAll(Console.WriteLine);
+
+		var options = new JsonSerializerOptions();
+		options.IncludeFields = true;
+		options.WriteIndented = true;
+
+		Mappings mappingsA;
+		using (var mappingsStream = File.Open("oldVersionMappings.json", FileMode.Open)) {
+			mappingsA = JsonSerializer.Deserialize<Mappings>(mappingsStream, options)!;
+		}
+		var mappingsB = new Mappings() {
+			NamespaceA = mappingsA.NamespaceA,
+			NamespaceB = mappingsA.NamespaceB,
+		};
+		Console.WriteLine(mappingsA.Classes.Count);
+		foreach (var classMapping in mappingsA.Classes) {
+			var classInstance = matcher.env.EnvA.types!.GetValueOrDefault(classMapping.ClassNameA, null);
+			if (classInstance == null) {
+				// TODO these missing instance cases evidently do happen; should investigate why
+				Console.WriteLine($"Missing class instance for {classMapping.ClassNameA}; this shouldn't happen probably?");
+				continue;
+			}
+			if (classInstance.HasMatch()) {
+				TypeInstance matchedClassInstance = classInstance.GetMatch()!;
+				var matchedClassMapping = new ClassMapping() {
+					ClassNameA = matchedClassInstance.GetName(),
+					ClassNameB = classMapping.ClassNameB,
+				};
+				mappingsB.Classes.Add(matchedClassMapping);
+				foreach (var methodMapping in classMapping.Methods) {
+					var methodInstance = classInstance.GetMethod(methodMapping.MethodNameA, null);
+					if (methodInstance == null) {
+						Console.WriteLine($"Missing method instance for {methodMapping.MethodNameA}; this shouldn't happen probably?");
+						continue;
+					}
+					if (methodInstance.HasMatch()) {
+						MethodInstance matchedMethodInstance = methodInstance.GetMatch()!;
+						var matchedMethodMapping = new MethodMapping() {
+							MethodNameA = matchedMethodInstance.GetName(),
+							MethodNameB = methodMapping.MethodNameB,
+						};
+						matchedClassMapping.Methods.Add(matchedMethodMapping);
+					}
+				}
+				foreach (var fieldMapping in classMapping.Fields) {
+					var fieldInstance = classInstance.GetField(fieldMapping.FieldNameA, null);
+					if (fieldInstance == null) {
+						Console.WriteLine($"Missing field instance for {fieldMapping.FieldNameA}; this shouldn't happen probably?");
+						continue;
+					}
+					if (fieldInstance.HasMatch()) {
+						FieldInstance matchedFieldInstance = fieldInstance.GetMatch()!;
+						var matchedFieldMapping = new FieldMapping() {
+							FieldNameA = matchedFieldInstance.GetName(),
+							FieldNameB = fieldMapping.FieldNameB,
+						};
+						matchedClassMapping.Fields.Add(matchedFieldMapping);
+					}
+				}
+			}
+		}
+		using (var mappingsStream = File.Open("newVersionMappings.json", FileMode.Create)) {
+			JsonSerializer.Serialize(mappingsStream, mappingsB, options);
+		}
 	}
 }
--- Matcher/Mapping.cs
+++ Matcher/Mapping.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace Matcher;
 
 public class Mappings {
@@ -17,13 +15,13 @@ public class ClassMapping {
 }
 
 public class FieldMapping {
-	public required string FieldTypeA;
+	// public required string FieldTypeA;
 	public required string FieldNameA;
 	public string? FieldNameB;
 }
 
 public class MethodMapping {
-	public required string ReturnTypeA;
+	// public required string ReturnTypeA;
 	public required string MethodNameA;
 	public string? MethodNameB;
 	public List<MethodParameterMapping> Parameters = [];
